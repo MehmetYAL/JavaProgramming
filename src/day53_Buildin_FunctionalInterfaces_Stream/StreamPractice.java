e4b7101cf2f519c32e90ec92d34e3b0ea530f6c9
Mehmet Yalcin <mehmetyalcin@hotmail.com>
2023-01-29 21:11:40 +0300

    day53 stream end of java

--- src/day53_Buildin_FunctionalInterfaces_Stream/StreamPractice.java
+++ src/day53_Buildin_FunctionalInterfaces_Stream/StreamPractice.java
@@ -19,7 +19,63 @@ public class StreamPractice {
         System.out.println(Arrays.toString(arr1));
 
         System.out.println("====================================");
+        //skip---if you write 4 inthe skip than  it doesnt print the first 4 elements
         List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        list2.stream().skip(5).collect(Collectors.toList());
+        System.out.println("list2 = " + list2);
+        int[] nums={1,2,3,4,5,6,7,8,9,10};
+        nums=Arrays.stream(nums).skip(4).toArray();
+        System.out.println("nums = " + Arrays.toString(nums));
+
+        System.out.println("------------------------------------------");
+        // limit--- it removes after given index number
+        List<Integer> list3=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+       list3=list3.stream().limit(6).collect(Collectors.toList());
+        System.out.println("list3 = " + list3);
+
+        //skip end limit together
+       list3= list3.stream().limit(7).skip(3).collect(Collectors.toList());
+        System.out.println("list3 = " + list3);
+
+        System.out.println("===========================================");
+
+        //map()------- can be use lambda expression of functional functionalinterfaces
+        List<Integer> list4=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+        List<String> days = new ArrayList<>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+        days=days.stream().map(p -> p.substring(0,3)).collect(Collectors.toList());
+        System.out.println("days = " + days);
+
+        //filter----
+        List<Integer> list5=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        List<Integer> evenNumbers = list5.stream().filter(p -> p % 2 == 0).collect(Collectors.toList());
+        System.out.println(evenNumbers);
+
+        System.out.println("=========================================");
+        //count  ----- method that how many are matched
+        List<Integer> list6=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13));
+
+        System.out.println(list6.stream().filter(p -> p % 2 == 0).count());
+
+        List<String> names = new ArrayList<>(Arrays.asList("Java", "java", "jAvA", "Python", "Ruby"));
+
+        System.out.println(names.stream().filter(p -> p.equalsIgnoreCase("java")).count());
+
+        //forEach
+        names.stream().filter(p -> p.equalsIgnoreCase("java")).forEach(p -> System.out.println(p));
+
+        //allMatch, anyMatch, nonMatch,
+        //if all elements is matched or if any element is matched or non of elements is matched than it uses boolean and return true or false
+        List<Integer> list7=new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        boolean r1=list7.stream().allMatch(p -> p%2==0);
+        System.out.println("r1 = " + r1);
+
+        boolean r2=list7.stream().anyMatch(p -> p > 20);
+        System.out.println("r2 = " + r2);
+
+        boolean r3=list7.stream().noneMatch(p -> p%3==0 );
+        System.out.println("r3 = " + r3);
+
 
     }
 }
